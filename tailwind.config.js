01c0956496378abc753bbc8af0e1b0488571dea0
muendokennedy <kennedymunyao999@gmail.com>
2023-09-20 08:02:19 +0300

    Preformatted the tailwind classes using prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
       screens: {
         'widescreen': {
           'raw': '(min-aspect-ratio: 3/2)'
         },
         'tallscreen': {
-          'raw': '(min-aspect-ratio: 13/20)'
+          'raw': '(max-aspect-ratio: 13/20)'
         },
       },
       keyframes: {
